ebc4d73e2a2fceadf1edf71ec5f9a5649823621e
Diaz <zaqxsw5467@naver.com>
2024-08-10 08:15:56 +0900

    초기 업로드

---
+++ talkwave-back/src/main/java/pj/talkwave/TalkwaveBackApplication.java
@@ -0,0 +1,13 @@
+package pj.talkwave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkwaveBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalkwaveBackApplication.class, args);
+	}
+
+}
